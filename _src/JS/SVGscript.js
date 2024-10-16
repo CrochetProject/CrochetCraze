093bf12755c171fa3455654a489e022b1b5d06c5
Savannah Ricks <savannahricksofficial123@gmail.com>
2024-10-16 18:24:54 -0400

    WORKING HOVERING

--- _src/JS/SVGscript.js
+++ _src/JS/SVGscript.js
@@ -1,55 +1,25 @@
 window.addEventListener('DOMContentLoaded', init, false)
-
-
-
 //GSsvg is ID of <object> containing Granny Square SVG
-
-// console.log("hello");
-
-
 function init(){
-
-    console.log("hello");
     let svgObject = document.getElementById('GSobject').contentDocument;
-// let svg = svgObject.contentDocument;
-    console.log(svgObject);
-
     let chain = svgObject.getElementsByClassName('chain');
-    console.log(chain.length);
     let doubleStitch = svgObject.getElementsByClassName('doubleStitch');
     let slipSt = svgObject.getElementsByClassName('slipSt');
-
-    console.log("page is fully loaded");
-    // console.log(chain.className);
-    // window.alert("hello");
-
     for ( let i = 0; i < chain.length; ++i ){
         chain[i].addEventListener('mouseover', stitchHighlight, false);
-        console.log("this chain has a listener");
+        chain[i].addEventListener('mouseout', stitchMouseOut, false);
     }
     for ( let i = 0; i < doubleStitch.length; ++i ){
         doubleStitch[i].addEventListener('mouseover', stitchHighlight, false);
-        console.log("this doubleStitch has a listener");
+        doubleStitch[i].addEventListener('mouseout', stitchMouseOut, false);
     }
     for ( let i = 0; i < slipSt.length; ++i ){
         slipSt[i].addEventListener('mouseover', stitchHighlight, false);
-        console.log("this slipSt has a listener");
+        slipSt[i].addEventListener('mouseout', stitchMouseOut, false);
     }
-    // event.stopPropagation();
-
 }
-
 function stitchHighlight(){
-    // console.log(this);
-    console.log("Inside stitchHighlight");
-    this.style.filter = "drop-shadow(0px 0px 25px rgb(245 237 22 / 1))";
-    this.style.stroke = "yellow";
-    this.style.strokeWidth = "10px";
-    this.addEventListener('mouseout', stitchMouseOut, false);
-
-
-
-
+    this.classList.toggle("on");
     const info = document.getElementById('DbStBubble');
     // document.addEventListener('mousemove', (e) => {
     //     info.style.display = "content";
@@ -67,13 +37,7 @@ function stitchHighlight(){
     //
     //     }, 20);
     // });
-
-
 }
-
 function stitchMouseOut(){
-    this.style.filter = "none";
-    // this.style.strokeWidth = "none";
-    this.style.stroke = "none";
+    this.classList.toggle("on");
 }
-
