7a898d92f961084f70e4242b8602e4d1f018734b
Savannah Ricks <savannahricksofficial123@gmail.com>
2024-12-10 15:10:19 -0500

    Updates

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
+
 function lastModified()
 {
-    console.log("hello");
     let modiDate=new Date(document.lastModified);
     let showAs=(modiDate.getMonth()+1)+ "/" +modiDate.getDate()+ "/" +modiDate.getFullYear();
     return showAs
@@ -10,4 +10,71 @@ document.write (lastModified() );
 document.write ("");
 
 //works ^^^
+
+
+
+function Menubar() {
+    var x = document.getElementById("navCont");
+    if (x.classList.contains("scrolled")) {
+        x.classList.toggle("responsive");
+    } else {
+        x.classList.toggle("responsive");
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const isHoverableDevice = window.matchMedia('(hover: hover) and (pointer: fine)');
+    console.log(isHoverableDevice);
+    let scrollTimeout;
+    let prevScrollPos = window.scrollY; //positionY before scroll
+    // console.log(`Before: ${prevScrollPos}`);
+    const menu = document.getElementById("navCont");
+
+    if(isHoverableDevice.matches === false) {
+        menu.classList.add("stick");
+        menu.style.padding = "0";
+    }
+    else{
+        window.addEventListener("scroll", function () {
+            clearTimeout(scrollTimeout);
+            scrollTimeout = setTimeout(function () {
+                const currentScrollPos = window.scrollY;//positionY after scroll
+                console.log(`After: ${currentScrollPos}`);
+
+                if (currentScrollPos > prevScrollPos) {
+                    menu.style.transform = "translateY(-100%)";//retract
+                } else {
+                    menu.style.transform = "translateY(0)";//stay
+                }
+
+                if (currentScrollPos > 0) {
+                    menu.classList.add("scrolled");
+                } else {
+                    menu.classList.remove("scrolled");
+                }
+                prevScrollPos = currentScrollPos;
+
+            }, 15);
+
+        });
+    }
+
+    let mybutton = document.getElementById("navCont");
+
+
+    $('a[href^="#"]').on("click", function (e) {
+        e.preventDefault();
+        var target = $(this.hash);
+        if (target.length) {
+            $("html, body").animate(
+                {
+                    scrollTop: target.offset().top,
+                },
+                500
+            );
+            Menubar(); // Toggle menu
+        }
+    });
+});
 
